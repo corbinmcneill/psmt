06e9baa6b02ceafe55ac0e4ba279fa2172440da5
calebclark <ccqwerty314@gmail.com>
2017-04-13 14:46:31 -0500

    public channel builds

--- mcio.c
+++ mcio.c
@@ -4,9 +4,11 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mcio.h"
 #include "pq.h"
 #include "debug.h"
+#include "params.h"
 
 #define MAX_BUF 1000
 #define MAX_THREADS 8
@@ -22,11 +24,22 @@ tppq pq;
 pthread_mutex_t done;
 pthread_mutex_t pq_lock;
 
+/* struct declarations */
+typedef struct public_trans_packet{
+    trans_packet tp;
+    int votes;
+    char* already_voted;
+    char not_empty;
+    char row_finished;
+} p_tp;
 
 
 /* function declarations */
 void *listener(void *threadid);
 int markreceivedround1(int seqnum, int wire,long* lastseqread,long* firstseqnotread, int* start_received, char* received); 
+void process_public(trans_packet tp, int wire, p_tp* public_cache, int start_pc, unsigned long firstpublicnotfinished);
+
+
 
 int mc_init(int* rfds_in,int* wfds_in, int n){
 
@@ -134,6 +147,21 @@ void* listener(void *threadid) {
     firstseqnotread = calloc(sizeof(long),numwires);
     received = calloc(MAX_MISORDERED*numwires*sizeof(char),1);
     start_received = calloc(numwires,sizeof(int));
+
+    unsigned long firstpublicnotfinished = 0;
+    //public_cache[(seq_num - firstpublicnotfinished)*wire] 
+    p_tp* public_cache = calloc(sizeof(p_tp)*numwires*MAX_MISORDERED,1);
+    int start_pc =0;
+    //initialize the public cache to empty
+    for (int i = 0; i < MAX_MISORDERED; i++) {
+        for (int j = 0; j < numwires; j++) {
+            public_cache[i*numwires+j].not_empty = 0;
+            public_cache[i*numwires+j].already_voted = calloc(sizeof(int)*numwires,1);
+        }
+    }
+
+    
+    
     for (int i = 0; i < numwires; i++) {
         lastseqread[i] = -1;
     }
@@ -157,7 +185,7 @@ void* listener(void *threadid) {
                     }
                 }
                 else {
-                    //processpublic(input->tp,i);
+                    process_public(input->tp,i,public_cache,start_pc,firstpublicnotfinished);
                 }
                     
            }
@@ -167,11 +195,18 @@ void* listener(void *threadid) {
 
        }
     }
+
+    for (int i = 0; i < MAX_MISORDERED; i++) {
+        for (int j = 0; j < numwires; j++) {
+            free(public_cache[i*numwires+j].already_voted);
+        }
+    }
     free(input);
     free(lastseqread);
     free(firstseqnotread);
     free(received);
     free(start_received);
+    free(public_cache);
     pthread_exit(NULL);
 }
 /*
@@ -180,13 +215,14 @@ void* listener(void *threadid) {
 int markreceivedround1(int seqnum, int wire,long* lastseqread,long* firstseqnotread, int* start_received, char* received) {
     debug("markreceived(%d,%d) called\n", seqnum, wire);
     debug("firstseqnotread[wire]=%d, lastseqread[wire]=%d\n", firstseqnotread[wire],lastseqread[wire] );
+    int to_return = -1;
     if (seqnum < firstseqnotread[wire]) {
-        return 0;
+        to_return = 0;
     } else if (seqnum == firstseqnotread[wire]) {
         int next;
         int i;
         int index;
-        for (i = 0; i < lastseqread[wire] - firstseqnotread[wire] ; i++) {
+        for (i = 1; i < lastseqread[wire] - firstseqnotread[wire] ; i++) {
             index =  (i+1+start_received[wire])%MAX_MISORDERED;
             if (!received[wire*index]) {
                 received[wire*index] = 0;
@@ -194,23 +230,28 @@ int markreceivedround1(int seqnum, int wire,long* lastseqread,long* firstseqnotr
             }
             received[wire*index] = 0;
         }
+        debug("moving firstseqnotread[%d] from %d to ", wire, firstseqnotread[wire]);
         firstseqnotread[wire] += i;
+        debug("%d\n",firstseqnotread[wire]);
+
         start_received[wire] = index;
         if (seqnum > lastseqread[wire]) {
             lastseqread[wire] = seqnum;
         }
-        return 1;
+        to_return = 1;
     } else if (seqnum < lastseqread[wire]) {
         int index = wire*((seqnum + start_received[wire]) % MAX_MISORDERED);
-        if (received[index] == 1)
-            return 0;
+        if (received[index] == 1) {
+            to_return = 0;
+        } else {
         received[index] = 1;
-        return 1;
+        to_return = 1;
+       }
     } else if (seqnum == lastseqread[wire]) {
-        return 0;
+        to_return = 0;
     } else {
         lastseqread[wire] = seqnum;
-        return 1;
+        to_return = 1;
     }
     debug("sending dummy if %d > %d\n",lastseqread[wire],firstseqnotread[wire] + MAX_MISORDERED);
     if (lastseqread[wire] > (firstseqnotread[wire] + MAX_MISORDERED)) {
@@ -225,9 +266,82 @@ int markreceivedround1(int seqnum, int wire,long* lastseqread,long* firstseqnotr
                debug("inserted dummy\n");
                //mark the packet as received
                markreceivedround1(dummy.tp.seq_num,wire, lastseqread,firstseqnotread,start_received,received);
-               return 1;
-        }
+               return to_return;
+    }
+    return to_return;
 
+}
+
+void cleanuprow(p_tp* public_cache, int row) {
+    public_cache[row].row_finished =1;
+    for (int i = 0; i < numwires; i++) {
+        p_tp* curr = &public_cache[row+i];
+        curr->not_empty = 0;
+        curr->votes = 0;
+        for (int j = 0; j < numwires; j++) {
+            curr->already_voted[j] = 0;
+        }
+    }
+}
+
+
+
+
+void process_public(trans_packet tp, int wire, p_tp* public_cache, int start_pc, unsigned long firstpublicnotfinished){
+    mc_packet temp;
+    temp.wire = -1;
+    int offset = tp.seq_num - firstpublicnotfinished;
+    // if it is out of range ignore it
+    if(offset < 0|| offset > MAX_MISORDERED) {
+       return; 
+    }
+
+    int start_index = (offset + start_pc)%MAX_MISORDERED;
+    for (int i = 0; i < numwires; i++) {
+        p_tp* curr = &public_cache[(start_index+i)%MAX_MISORDERED];
+        // if this voting block is empty 
+        if (!curr->not_empty) {
+            curr->not_empty = 1;
+            curr->votes = 1;
+            curr->tp = tp;
+            curr->already_voted[wire] =1;
+            break;
+        } else {
+            // if the packet matches one already in the array
+            if (!memcmp(&(curr->tp), &tp, sizeof(trans_packet))){
+                // the wire has already voted, drop the packet
+                if (curr->already_voted[wire]) {
+                    return;
+                }
+                // cast a vote
+                curr->votes++;
+                curr->already_voted[wire] = 1;
+                // see if we have enough votes
+                if (curr->votes > T) {
+                    // if we have enough votes, insert the trans_packet
+                    temp.tp = tp;  
+                    safe_insert(&temp);
+                    cleanuprow(public_cache,start_index);
+                    // if we are the firstpublicnotfinished, find a new one
+                    if (firstpublicnotfinished == tp.seq_num) {
+                        int j;
+                        int index;
+                        
+                        for (j = 1; j < MAX_MISORDERED; j++) {
+                            index = (start_pc + j*numwires)%MAX_MISORDERED;
+                            if (!public_cache[index].row_finished){
+                                public_cache[index].row_finished = 0;
+                                start_pc = index;
+                                break;
+                            }
+                            public_cache[index].row_finished = 0;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
 }
        
            
