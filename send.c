b60cf6591683abe7cb1f35a755a454f3d671b3c3
Corbin McNeill <corbin.mcneill@my.wheaton.edu>
2017-04-13 11:17:40 -0500

    fixed calls to send_spin and receive_spin

--- send.c
+++ send.c
@@ -1,4 +1,3 @@
-#include "psmt.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -7,6 +6,8 @@
 #include <string.h>
 #include <errno.h>
 #include <pthread.h>
+
+#include "psmt.h"
 
 char *message = "test message\n"; 
 unsigned int message_n = 13;
@@ -33,10 +34,10 @@ int main() {
 	/* Note: with proper synchronization, this code could
 	 * (and should) be extended later on to support multiple
 	 * spinning threads. */
-	pthread_t sender_spin;
+	pthread_t send_spin_thread;
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	if (pthread_create(&sender_spin, &attr, send_spin, NULL)) {
+	if (pthread_create(&send_spin_thread, &attr, &send_spin, NULL)) {
 		printf("send error: thread_create - %s", strerror(errno));
 		return 1;
 	}
@@ -45,7 +46,7 @@ int main() {
 		send_char(message[i]);
 	}
 	
-	pthread_cancel(&sender_spin);
+	pthread_cancel(send_spin_thread);
 	pthread_attr_destroy(&attr);
 
 	return 0;
